2a9afd9e840a80a4243269db1d1b25df16bf7b5b
eblondel <emmanuel.blondel1@gmail.com>
2022-05-13 10:20:06 +0200

    control over vre_context

--- global.R
+++ global.R
@@ -24,7 +24,7 @@ fetchProfile <- function(jwt){
   out_jwt$jwt <- jwt
   
   vre_contexts <- names(out_jwt$resource_access)[startsWith(names(out_jwt$resource_access), "%2Fd4science.research-infrastructures.eu")]
-  if(length(vre_context)) stop("No VRE context available!")
+  if(length(vre_context)==0) stop("No VRE context available!")
   out_jwt$vre_context <- vre_context[[1]]
   out_jwt$vre_resource_access <- out_jwt$resource_access[[out_jwt$vre_context]]
   out_jwt$shiny_resource_access <- out_jwt$resource_access[["dcf-shiny"]]
