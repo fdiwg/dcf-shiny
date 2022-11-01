a79ef49cc7cd469df4c0c63afe6718a1e35a0c54
eblondel <emmanuel.blondel1@gmail.com>
2022-11-01 23:34:07 +0100

    initAppDataspace

--- assets/d4s_utils.R
+++ assets/d4s_utils.R
@@ -114,3 +114,23 @@ initAppWorkspace <- function(config, profile, components){
   }
   return(ws)
 }
+
+#initAppDataspace
+initAppDataspace <- function(config, profile, components){
+  if(is.null(profile$access)) return(NULL)
+  pool <- components$POOL
+  SH <- components$STORAGEHUB
+  
+  ws <- NULL
+  if(is(SH, "StoragehubManager")){
+    ws <- SH$getWSItemID( parentFolderID = SH$getWSVREFolderID(), itemPath = config$dcf$workspace )
+    if(is.null(ws)){
+      ws <- SH$createFolder(folderID = SH$getWSVREFolderID(),name = config$dcf$workspace, description = sprintf("Dataspace for the '%s' application", config$dcf$workspace))
+      if(is.null(ws)){
+        ERROR("Failed to create app dataspace '%s'", config$dcf$workspace)
+        stop(sprintf("Failed to create app dataspace '%s'", config$dcf$workspace))
+      }
+    }
+  }
+  return(ws)
+}
--- server.R
+++ server.R
@@ -6,6 +6,9 @@ server <- function(input, output, session) {
   CONFIG$dcf$user_workspace <- sprintf("%s-%s", CONFIG$dcf$workspace, PROFILE$preferred_username)
   CONFIG$workspace_id <- initAppWorkspace(config = CONFIG, profile = PROFILE, components = COMPONENTS)
   
+  #initAppDataspace
+  CONFIG$dataspace_id <- initAppDataspace(config = CONFIG, profile = PROFILE, components = COMPONENTS)
+  
   #enrich profile with reporting entities
   PROFILE$reporting_entities <- getDBUserReportingEntities(profile = PROFILE, pool = COMPONENTS$POOL)
   
