eb89417ea6a6ad4008085104a9d72a52e13fd8bf
eblondel <emmanuel.blondel1@gmail.com>
2022-11-01 23:34:35 +0100

    persist wecafc-firms data

--- modules/plugins/wecafc-firms/wecafc-firms_publisher_server.R
+++ modules/plugins/wecafc-firms/wecafc-firms_publisher_server.R
@@ -40,22 +40,66 @@ function(id, parent.session, config, profile, components){
         return(newdata)
       }
       
-      #storeData
-      storeData <- function(config, pool, profile, store, task_id){
+      #combineData
+      combineData <- function(newdata, dbdata){
+        out <- rbind(newdata,dbdata)
+        out$duplicated <- duplicated(out[,colnames(out)[!colnames(out) %in% c("measurement_value")]])
+        out <- out[!out$duplicated,]
+        out$duplicated <- NULL
+        return(out)
+      }
+      
+      #storeInDataspace
+      storeInDataspace <- function(config, pool, profile, store, task_id, datafile){
+        
+        #check existance of task id folder, if not create it
+        task_folder_id <- store$getWSItemID(parentFolderID = config$dataspace_id, itemPath = task_id)
+        if(is.null(task_folder_id)){
+          store$createFolder(folderID = config$dataspace_id, name = task_id)
+          task_folder_id <- store$getWSItemID(parentFolderID = config$dataspace_id, itemPath = task_id)
+        }
+        
+        #store data
+        datafileID <- store$uploadFile(folderID = task_folder_id, file = datafile)
+        return(datafileID)
+      }
+      
+      #persistData
+      persistData <- function(config, pool, profile, store, task_id){
+        datetime <- format(Sys.time(),paste0("%Y%m%d%H%M%S"))
         table_id <- tolower(gsub("-", "_", gsub("\\.", "_", task_id)))
         
         #read data for latest open data call
         newdata <- readAndMergeAcceptedData(config, pool, profile, store, task_id)
         
-        #TODO store new data in a workspace for data manager
-        #TODO store db backup in workspace
-        #TODO store new data in the database
+        #store new data in dataspace
+        newdata_file <- paste0(datetime, "_new.csv")
+        readr::write_csv(newdata, newdata_file)
+        storeInDataspace(config, pool, profile, store, task_id, newdata_file)
+        unlink(newdata_file)
+        
+        #store db backup in dataspace
+        dbdata <- NULL
         hasData <- DBI::dbExistsTable(pool, table_id, schema = "public")
         if(hasData){
-          #TODO data review/overwrite procedure
-        }else{
-          #TODO include data
+          dbdata <- DBI::dbReadTable(pool, table_id, schema = "public")
+          dbdata_file <- paste0(datetime, "_db_old.csv")
+          readr::write_csv(dbdata, dbdata_file)
+          storeInDataspace(config, pool, profile, store, task_id, dbdata_file)
+          unlink(dbdata_file)
         }
+        
+        #combineData
+        dbdatanew <- combineData(newdata, dbdata)
+        
+        #store db new version in workspace
+        dbdatanew_file <- paste0(datetime, "_db_new.csv")
+        readr::write_csv(dbdatanew, dbdatanew_file)
+        storeInDataspace(config, pool, profile, store, task_id, dbdatanew_file)
+        unlink(dbdatanew_file)
+        
+        #store new data in the database
+        DBI::dbWriteTable(conn = pool, name = table_id, value = dbdatanew, overwrite = TRUE)
         
       }
       
