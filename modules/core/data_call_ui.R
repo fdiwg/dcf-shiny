8249e57679d1590e9bd1be3d6172d2ef252e9ffc
Bennici <alexandre-ben@hotmail.fr>
2022-11-30 17:54:23 +0100

    #37 datacall module update

--- modules/core/data_call_ui.R
+++ modules/core/data_call_ui.R
@@ -5,6 +5,17 @@ data_call_ui <- function(id){
   shinydashboard::tabItem(
     tabName = "data_call",
     shiny::actionButton(inputId = ns("add_data_call"), label = "Add a new data call", class = "btn-primary"),
+    br(),br(),
+    fluidRow(
+      div(
+        class = "col-md-2",
+        uiOutput(ns("task_selector"))
+      ),
+      div(
+        class = "col-md-2",
+        uiOutput(ns("status_selector"))
+      )
+    ),
     DT::DTOutput(ns("tbl_data_calls"))
   )
 }
