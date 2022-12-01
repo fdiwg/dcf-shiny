c73d2d54e1b35a543ba1d674807c2a25fe730725
eblondel <emmanuel.blondel1@gmail.com>
2022-12-01 15:39:34 +0100

    fine-tune data task datasets download

--- modules/plugins/wecafc-firms/wecafc-firms_manager_server.R
+++ modules/plugins/wecafc-firms/wecafc-firms_manager_server.R
@@ -10,10 +10,10 @@ function(id, parent.session, config, profile, components){
       getRDBDatasets <- function(){
         task_folders <- SH$listWSItems(config$dataspace_id)
         tasks <- data.frame(
-          "id" = task_folders$name,
-          "name" = sapply(task_folders$name, function(x){config$dcf$tasks[[x]]$name}),
-          "creationTime" = as.POSIXct(task_folders$creationTime/1000, origin = "1970-01-01"),
-          "lastModificationTime" = sapply(task_folders$id, function(x){
+          "task_id" = task_folders$name,
+          "task_name" = sapply(task_folders$name, function(x){config$dcf$tasks[[x]]$name}),
+          "creation_date" = as.POSIXct(task_folders$creationTime/1000, origin = "1970-01-01"),
+          "update_date" = sapply(task_folders$id, function(x){
             max(SH$listWSItems(parentFolderID = x)$lastModificationTime)
           }),
           "actions" = sapply(task_folders$id, function(x){
@@ -24,37 +24,73 @@ function(id, parent.session, config, profile, components){
             SH$getPublicFileLinkByID(items$id)
           })
         )
-        tasks$lastModificationTime <- as.POSIXct(tasks$lastModificationTime/1000, origin = "1970-01-01")
+        tasks$update_date <- as.POSIXct(tasks$update_date/1000, origin = "1970-01-01")
         return(tasks)
       }
       
-      output$tbl_rdb_datasets <- DT::renderDT(
-        getRDBDatasets(),
-        selection='single', escape=FALSE,rownames=FALSE,
-        options=list(
-          lengthChange = FALSE,
-          paging = FALSE,
-          searching = FALSE,
-          preDrawCallback = JS(
-            'function() {
-                  Shiny.unbindAll(this.api().table().node()); }'
-          ),
-          drawCallback = JS('function() {
-                        Shiny.bindAll(this.api().table().node()); }'
-          ),
-          autoWidth = FALSE,
-          columnDefs = list(
-            list(width = '400px', targets = c(4),
-                 render = JS("function(data, type, full, meta) {
-                           var html = data;
-                           if(data.startsWith(\"http://\") | data.startsWith(\"https://\")){
-                              html = '<a href=\"' + data + '\" target=\"_blank\">Download</a>';
-                           }
-                           return html;
-                        }"))
+      #dcTableHandler
+      rdbTableHandler <- function(data){
+        
+        if(nrow(data)>0){
+          data <- do.call("rbind", lapply(1:nrow(data), function(i){
+            out_tib <- tibble::tibble(
+              "Task ID" = data[i,"task_id"],
+              "Task name" = data[i,"task_name"],
+              "Creation date" = data[i, "creation_date"],
+              "Update date" = data[i,"update_date"],
+              "Actions" = data[i,"actions"]
+            )
+            return(out_tib)
+          }
+          ))
+        }else{
+          data <- tibble::tibble( 
+            "Task ID" = character(0),
+            "Task name" = character(0),
+            "Creation date" = character(0),
+            "Update date" = character(0),
+            "Actions" = character(0)
+          )
+        }
+        return(data)
+      }
+      
+      renderRDBDatasets <- function(data){
+      
+        output$tbl_rdb_datasets <- DT::renderDT(
+          datatable(rdbTableHandler(data),
+            selection='single', escape=FALSE,rownames=FALSE,
+            options=list(
+              lengthChange = FALSE,
+              paging = FALSE,
+              searching = FALSE,
+              preDrawCallback = JS(
+                'function() {
+                      Shiny.unbindAll(this.api().table().node()); }'
+              ),
+              drawCallback = JS('function() {
+                            Shiny.bindAll(this.api().table().node()); }'
+              ),
+              autoWidth = FALSE,
+              columnDefs = list(
+                list(width = '400px', targets = c(4),
+                     render = JS("function(data, type, full, meta) {
+                               var html = data;
+                               if(data.startsWith(\"http://\") | data.startsWith(\"https://\")){
+                                  html = '<a href=\"' + data + '\" target=\"_blank\">Download</a>';
+                               }
+                               return html;
+                            }"))
+              )
+            )
           )
         )
-      )
+      }
+        
+      #render tables
+      observe({
+        renderRDBDatasets(getRDBDatasets())
+      })
       
       #-----------------------------------------------------------------------------------
     }
